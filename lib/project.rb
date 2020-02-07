794a432d8bf85c20853bff26d673de0d6d3342bd
Agnes Park <agnespark824@gmail.com>
2020-02-07 05:28:03 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,1 +1,9 @@
 #projects can have many backers and backers can back many projects
+
+class Project
+  attr_accessor :backers
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
