e799ede83a51151e8f1c8700b0b35bbb9a5c18bf
Agnes Park <agnespark824@gmail.com>
2020-02-07 05:29:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,4 +7,10 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
+  end
   
+  def back_project(project)
+    @backed_projects << project
+    project.backers 
+  end
+end
