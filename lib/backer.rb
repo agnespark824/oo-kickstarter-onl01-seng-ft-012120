e638cb390c6ba3e996620bd486c49a21cc17cc1c
Agnes Park <agnespark824@gmail.com>
2020-02-07 05:25:22 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,1 +1,22 @@
 #projects can have many backers and backers can back many projects
+
+describe 'Backer - ::new' do
+  it 'takes a name on initialization, accessible through an attribute reader' do
+    backer = Backer.new("Avi")
+    expect(backer.name).to eq("Avi")
+  end
+
+  it 'initializes with a @backed_projects attribute, an empty array' do
+    backer = Backer.new("Avi")
+    expect(backer.backed_projects).to eq([])
+  end
+end
+
+class Backer
+  attr_accessor
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  
